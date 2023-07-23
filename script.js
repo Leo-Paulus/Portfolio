e69cb289cd9674702c163fbc3b0917914f5f79bc
LeonelPaulus <leopauluss@gmail.com>
2023-07-23 18:49:09 -0300

    WIP: darkmode

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+/*TODO: when the hamburger nav is present, the sun doesn't change to moon. */
+function toggleMode() {
+    const toggle = document.querySelector('.toggleDark');
+    const body = document.querySelector('body');
+    const hamburger = document.querySelector('.menu-links');
+    const links = document.querySelectorAll('a');
+    toggle.classList.toggle('bi-moon');
+        if(toggle.classList.toggle('bi-brightness-high-fill')){
+            body.style.background = 'white';
+            body.style.color = 'black';
+            body.style.transition = '2s';
+            links.forEach(link => link.style.color = 'black');
+            links.forEach(link => link.style.transition = '2s');
+            hamburger.style.background = 'white';
+            hamburger.style.transition = '2s';
+        }else{
+            body.style.background = 'black';
+            body.style.color = 'white';
+            body.style.transition = '2s';
+            links.forEach(link => link.style.color = 'white');
+            links.forEach(link => link.style.transition = '2s');
+            hamburger.style.transition = '2s';
+            hamburger.style.background = 'black';
+
+            
+        }
+}
